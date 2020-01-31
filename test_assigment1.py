464720c2f5c03bdce1e0b9fbfabfd769bfe2cd52
ZhaoPeixi627 <54923817+ZhaoPeixi627@users.noreply.github.com>
2020-01-30 21:17:55 -0500

    Update test_assigment1.py

--- test_assigment1.py
+++ test_assigment1.py
@@ -3,7 +3,7 @@ from assigment1 import arabicToRoman
 def test_arabicToRoman():
     assert arabicToRoman(1) == 'I'
     assert arabicToRoman(2) == 'II'
-    assert arabicToRoman(2) == 'III'  
+    assert arabicToRoman(3) == 'III'  
     assert arabicToRoman(3999) == 'MMMCMXCIX'
     assert arabicToRoman(123) == 'CXXIII' 
     assert arabicToRoman(4) == 'IV'
