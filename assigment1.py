e67bf6bc5d9381383aeb170186c1ddbe360bcc53
ZhaoPeixi627 <54923817+ZhaoPeixi627@users.noreply.github.com>
2020-02-26 21:37:09 -0500

    Update assigment1.py

--- assigment1.py
+++ assigment1.py
@@ -1,16 +1,8 @@
 # copyright Peixi Zhao  bradzhao@bu.edu
 
 def arabicToRoman(num):
-    """
-    :type num: int
-    :rtype: str
-    """
-    try:
-        int(num)
-    except(ValueError):
-        print("Please input a valid arabic integer")
+    if not isinstance(num,int):
         return -1
-    
     arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
     res = ''
